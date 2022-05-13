f0eedbe606eb410387f6cfc1cd99cbb6abb5fafa
yasumayu <yasuda.mayu304@mail.kyutech.jp>
2022-05-13 09:05:34 +0900

    Pillowでの画像の読み込みをtifffileに変更

--- tiff_h5/tifftoh5.py
+++ tiff_h5/tifftoh5.py
@@ -5,16 +5,15 @@ import os
 import os.path
 import sys
 import numpy as np
-from PIL import Image
+import tifffile as tif
 
-
-#ラベルデータ,生データのパス入力
+# ラベルデータ,生データのパス入力
 input_label_path = sys.argv[1]
 input_raw_path = sys.argv[2]
-#生データのディレクトリ名の取得(.tiffを除いたもの)
+# 生データのディレクトリ名の取得(.tiffを除いたもの)
 name = os.path.basename(input_raw_path)[0:-5]
 h5_path = f'./{name}.h5'
-#h5_path = "./dualtomo.h5"
+# h5_path = "./dualtomo.h5"
 
 """""
 (name).h5
@@ -24,18 +23,14 @@ h5_path = f'./{name}.h5'
 
 """
 
-#HDF5ファイルの作成
+# HDF5ファイルの作成
 with h5py.File(h5_path, "w") as f:
-    
-    #ラベルデータのtiffファイルの読み込み
-    labeldata = np.array(Image.open(input_label_path))
+    # ラベルデータのtiffファイルの読み込み
+    labeldata = np.array(tif.imread(input_label_path))
     label_datasets = f.create_dataset(name='label', data=labeldata, dtype=labeldata.dtype)
 
-
-    #生データのtiffファイルを読み込み
-    rawdata = np.array(Image.open(input_raw_path))
-    raw_datasets  = f.create_dataset(name='raw', data=rawdata, dtype=rawdata.dtype)
-    
+    # 生データのtiffファイルを読み込み
+    rawdata = np.array(tif.imread(input_raw_path))
+    raw_datasets = f.create_dataset(name='raw', data=rawdata, dtype=rawdata.dtype)
 
 
-   
--- tiff_h5/tifftoh5_multi.py
+++ tiff_h5/tifftoh5_multi.py
@@ -5,7 +5,8 @@ import os
 import os.path
 import sys
 import numpy as np
-from PIL import Image
+import tifffile as tif
+
 
 input_raw_path = sys.argv[1]
 
@@ -33,25 +34,25 @@ h5_path = f'./multi_{name}.h5'
 with h5py.File(h5_path, "w") as f:
 
     #生データのtiffファイルを読み込み
-    rawdata = np.array(Image.open(input_raw_path))
+    rawdata = np.array(tif.imread(input_raw_path))
     raw_datasets  = f.create_dataset(name='raw', data=rawdata, dtype=rawdata.dtype)
 
     label = f.create_group('label')
     
     #アクチンのアノテーションを行ったtiffファイルの読み込み
-    actindata = np.array(Image.open(input_actin_path))
+    actindata = np.array(tif.imread(input_actin_path))
     actin_datasets = f.create_dataset('label/actin', data=actindata, dtype=actindata.dtype)
 
     #ミオシンのアノテーションを行ったtiffファイルの読み込み
-    myosindata = np.array(Image.open(input_myosin_path))
+    myosindata = np.array(tif.imread(input_myosin_path))
     myosin_datasets = f.create_dataset('label/myosin', data=myosindata, dtype=myosindata.dtype)
 
     #金コロイドのアノテーションを行ったtiffファイルの読み込み
-    colloiddata = np.array(Image.open(input_colloid_path))
+    colloiddata = np.array(tif.imread(input_colloid_path))
     colloid_datasets = f.create_dataset('label/colloid', data=colloiddata, dtype=colloiddata.dtype)
 
     #ミトコンドリアのアノテーションを行ったtiffファイルの読み込み
-    mitochondata = np.array(Image.open(input_mitochon_path))
+    mitochondata = np.array(tif.imread(input_mitochon_path))
     mitochon_datasets = f.create_dataset('label/mitochon', data=mitochondata, dtype=mitochondata.dtype)
 
 
