0076fdd0f4371c4d8e16b3e344b6a2ae9f73a7d6
yasumayu <yasuda.mayu304@mail.kyutech.jp>
2022-12-19 11:23:13 +0900

    出力テキストファイル変更

--- evaluation/multi/eval_actin_multi_add_negative.py
+++ evaluation/multi/eval_actin_multi_add_negative.py
@@ -135,7 +135,7 @@ def main():
 
         precision, recall, iou, f1, f1_3,f1_5  = evaluate(sum_tp, sum_tn, sum_fp, sum_fn)
 
-        with open('threshold_actin_multi.txt', 'a') as f:
+        with open('threshold_actin_multi_add_negative.txt', 'a') as f:
             f.write(f'Threshold:{thresh}\n')
             f.write(f'TN:{sum_tn} ')
             f.write(f'TP:{sum_tp} ')
